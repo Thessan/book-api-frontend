2550c5658e83eaa0358e0a8555aa5c0beefd75f1
Therese <64402455+Thessan@users.noreply.github.com>
2020-12-14 09:55:26 +0100

    updated FirstPage

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import './firstpage.css'
+import styled from 'styled-components'
 
 import { NavigationBar } from './NavigationBar'
 
@@ -8,7 +7,41 @@ export const FirstPage = () => {
     return (
         <>
         <NavigationBar />
-        <section className="welcome"></section>
+        <QuoteContainer>
+            <Quote>“That’s the thing about books. They let you travel without moving your feet.”</Quote>
+        </QuoteContainer>
+            <QuoteAuthorContainer>
+                <QuoteAuthor>– Jhumpa Lahiri</QuoteAuthor>
+            </QuoteAuthorContainer>
+        
         </>
     )
 }
+
+const QuoteContainer = styled.div`
+text-align: center;
+margin: auto;
+padding-top: 250px;
+margin-bottom: 0;
+`
+
+const Quote = styled.h1`
+font-family: 'Goudy Bookletter 1911', serif;
+font-size: 35px;
+color: white;
+margin: auto;
+padding: 8px;
+`
+
+const QuoteAuthorContainer = styled.div`
+font-family: 'Goudy Bookletter 1911', serif;
+font-size: 20px;
+color: white;
+text-align: center;
+font-style: italic;
+margin: auto;
+`
+
+const QuoteAuthor = styled.p`
+font-size: 21px;
+`
